7ec7c28e727a4660b1cfd11b98dbb7b9052978d7
Mihai Cimpoesu <mihai.cimpoesu@gmail.com>
2016-08-27 21:09:22 +0100

    Adding my own version of crypto-js to the shims.js

--- shims.js
+++ shims.js
@@ -5,7 +5,7 @@ module.exports = {
   "inherits": "^2.0.1",
   "console-browserify": "^1.1.0",
   "constants-browserify": "~0.0.1",
-  "crypto-js": "^3.1.6",
+  "crypto-js": "imcu/crypto-js",
   "react-native-randombytes": "^2.0.0",
   "dns.js": "^1.0.1",
   "domain-browser": "^1.1.1",
